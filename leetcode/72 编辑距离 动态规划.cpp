861e15d3cc2e1633f51ca70155c8f493cc614431
KellyGong <gongzh666@gmail.com>
2019-05-29 10:01:55 +0800

    Create 72 编辑距离 动态规划.cpp

---
+++ leetcode/72 编辑距离 动态规划.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.size();
+        int len2 = word2.size();
+        vector<vector<int>> dp = vector(len1 + 1, (vector<int>(len2 + 1, 1000000)));
+        for(int i = 0; i <= len2; i++){
+            dp[0][i] = i;
+        }
+        for(int i = 0; i <= len1; i++){
+            dp[i][0] = i;
+        }
+        for(int i = 1; i <= len1; i++){
+            for(int j = 1; j <= len2; j++){
+                if(word1[i - 1] == word2[j - 1]) dp[i][j] = min(dp[i - 1][j - 1], dp[i][j]);
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i][j]);
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+                    dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        
+        
+        return dp[len1][len2];
+    }
+};
